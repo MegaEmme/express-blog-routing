6e601c7fc4c4e62c4f3dc86e942dd6a72438264a
MegaEmme <mikmusco@gmail.com>
2025-03-27 13:13:10 +0100

    exported routers/posts

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const app = express();
 
 const port = 4000;
 
+const dolci = require('./routers/posts');
+
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     console.log('Server del mio blog');
     res.send('<h1>Server del mio blog</h1>');
 });
+
+app.use("/posts", dolci);
 
 app.listen(port, ()=> {
     console.log('sono un server attivo sulla porta ' + port);
