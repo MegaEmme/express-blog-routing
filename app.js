1c9ad0ed6a45ef2c6ff32b812fb0f549b17f8122
MegaEmme <mikmusco@gmail.com>
2025-03-27 13:03:05 +0100

    feat: create routers folder

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Mike's routing");
+const express = require('express');
+
+const app = express();
+
+const port = 4000;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    console.log('Server del mio blog');
+    res.send('<h1>Server del mio blog</h1>');
+});
+
+app.listen(port, ()=> {
+    console.log('sono un server attivo sulla porta ' + port);
+})
